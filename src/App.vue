bb3fb65752b51f4c7f954e3551b90f25f995d357
em2046 <em2046@qq.com>
2020-05-17 22:37:50 +0800

    demo

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,46 @@
 <template>
-  <NovaColorPicker></NovaColorPicker>
+  <div>{{ state.color }}</div>
+  <NovaColorPicker
+    :preset="preset"
+    :value="state.color"
+    @update="onUpdate"
+    alpha
+  ></NovaColorPicker>
 </template>
 
 <script>
+import { reactive } from 'vue'
 import { NovaColorPicker } from '@em2046/nova-next'
 import '@em2046/nova-next/dist/css/color-picker.css'
 
+const preset = [
+  '#dc143c',
+  '#f0e68c',
+  '#8fbc8f',
+  '#00ced1',
+  '#1e90ff',
+  '#663399',
+  '#708090'
+]
+
 export default {
   components: {
-    NovaColorPicker,
+    NovaColorPicker
   },
+  setup() {
+    const state = reactive({
+      color: '#663399'
+    })
+
+    function onUpdate(color) {
+      state.color = color
+    }
+
+    return {
+      state,
+      onUpdate,
+      preset
+    }
+  }
 }
 </script>
